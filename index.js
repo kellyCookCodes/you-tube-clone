4b05aaccc6bc833f6f8c4826a92b5b7b160e45db
Geewiz <106394210+kellyCookCodes@users.noreply.github.com>
2025-12-17 18:30:54 -0800

    Add bootstrap badges with active and non-active DOM states, set cursor to pointer, font family, sm screen media query, and badge functionality that andd query to the search bar

--- index.js
+++ index.js
@@ -26,11 +26,30 @@ function displayVideos(data) {
 
     const { videoId } = video.id
     iframe.src = `https://www.youtube.com/embed/${videoId}`
-    iframe.allow = allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     iframe.setAttribute('allowfullscreen', 'true')
 
     colDiv.append(iframe)
     videosList.append(colDiv)
+  })
+}
+
+const badges = document.getElementsByClassName('badge')
+Array.from(badges).forEach(badge => {
+  badge.addEventListener('click', (event) => {
+    removeActiveSelection()
+    badge.classList.add('text-bg-light')
+    badge.classList.remove('text-bg-dark')
+    document.getElementById('search-text').value = badge.innerHTML
+    console.log(badge.innerText)
+    searchYTVideos()
+  })
+})
+
+function removeActiveSelection() {
+  Array.from(badges).forEach(badge => {
+    badge.classList.remove('text-bg-light')
+    badge.classList.add('text-bg-dark')
   })
 }
 
